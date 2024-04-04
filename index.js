02bd7b85dba989b8aa82322a86322484a57a715a
Nazlin <jemelinazlin@gmail.com>
2024-04-04 08:53:18 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+}
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+        console.log(i);
+    }
+}
